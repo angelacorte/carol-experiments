a099dc4d2ccf194b4bfb2795c114f1d51caa01e6
Angela Cortecchia <angela.cortecchia@unibo.it>
2026-03-13 11:05:28 +0100

    feat: create Obstacle avoidance cbf

---
+++ src/main/kotlin/it/unibo/collektive/control/cbf/ObstacleAvoidanceCBF.kt
@@ -0,0 +1,40 @@
+package it.unibo.collektive.control.cbf
+
+import com.gurobi.gurobi.GRB
+import com.gurobi.gurobi.GRBModel
+import com.gurobi.gurobi.GRBVar
+import it.unibo.collektive.control.ControlFunctionContext
+import it.unibo.collektive.model.Obstacle
+import it.unibo.collektive.model.minus
+import it.unibo.collektive.model.squaredNorm
+import it.unibo.collektive.model.toDoubleArray
+import it.unibo.collektive.solver.gurobi.ConstraintNames
+import it.unibo.collektive.solver.gurobi.GRBVector
+import it.unibo.collektive.solver.gurobi.toLinExpr
+import kotlin.math.pow
+
+/**
+ * Obstacle-avoidance barrier under ZOH dynamics;
+ * adds a keep-out CBF against a static obstacle.
+ */
+class ObstacleAvoidanceCBF(val obstacle: Obstacle, override val eta: Double = 0.5, override val slackWeight: Double? = null) : CBF() {
+    override val name: String = "obstacle_avoidance"
+    override var slack: GRBVar? = null
+
+    override fun GRBModel.applyCBF(uSelf: GRBVector, uOther: GRBVector?, context: ControlFunctionContext) {
+        val distance = (context.self.position - obstacle).toDoubleArray()
+        val safeDistance = obstacle.radius + obstacle.margin
+        val h = distance.squaredNorm() - safeDistance.pow(2)
+        val dt = context.settings.deltaTime
+        // RHS: -(\eta / \Delta t) * h_{i,k}^{obs}
+        val rhs = -(eta / dt) * h
+        // LHS: 2 * (p_i - p_o)^T * u_i
+        val lhs = uSelf.toLinExpr(distance, 2.0)
+        if (slackWeight != null) { // Soften the constraint if a slack weight was specified
+            val s = addVar(0.0, GRB.INFINITY, 0.0, GRB.CONTINUOUS, ConstraintNames.slack(name))
+            slack = s
+            lhs.addTerm(1.0, s)
+        }
+        addConstr(lhs, GRB.GREATER_EQUAL, rhs, ConstraintNames.obstacle("local"))
+    }
+}
