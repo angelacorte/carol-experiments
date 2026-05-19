174a42a3d59c447df4ac1232f3eff0e37af380b2
Angela Cortecchia <angela.cortecchia@unibo.it>
2026-05-19 18:52:49 +0200

    refactor: rename ScalarExpression to RuntimeScalar

--- src/main/kotlin/it/unibo/collektive/control/dsl/ConstraintFormula.kt
+++ src/main/kotlin/it/unibo/collektive/control/dsl/ConstraintFormula.kt
@@ -9,40 +9,40 @@ enum class FormulaSense(internal val gurobiSense: Char) {
 
 sealed interface ConstraintFormula {
     val sense: FormulaSense
-    val rightHandSide: ScalarExpression
+    val rightHandSide: RuntimeScalar
 }
 
 class LinearConstraintFormula internal constructor(
     val leftHandSide: AffineExpression,
     override val sense: FormulaSense,
-    override val rightHandSide: ScalarExpression,
+    override val rightHandSide: RuntimeScalar,
 ) : ConstraintFormula
 
 class QuadraticConstraintFormula internal constructor(
     val leftHandSide: QuadraticExpression,
     override val sense: FormulaSense,
-    override val rightHandSide: ScalarExpression,
+    override val rightHandSide: RuntimeScalar,
 ) : ConstraintFormula
 
-infix fun AffineExpression.lessThanOrEqualTo(rightHandSide: ScalarExpression): ConstraintFormula =
+infix fun AffineExpression.lessThanOrEqualTo(rightHandSide: RuntimeScalar): ConstraintFormula =
     LinearConstraintFormula(this, FormulaSense.LessThanOrEqualTo, rightHandSide)
 
 infix fun AffineExpression.lessThanOrEqualTo(rightHandSide: Double): ConstraintFormula =
     this lessThanOrEqualTo scalar(rightHandSide)
 
-infix fun AffineExpression.greaterThanOrEqualTo(rightHandSide: ScalarExpression): ConstraintFormula =
+infix fun AffineExpression.greaterThanOrEqualTo(rightHandSide: RuntimeScalar): ConstraintFormula =
     LinearConstraintFormula(this, FormulaSense.GreaterThanOrEqualTo, rightHandSide)
 
 infix fun AffineExpression.greaterThanOrEqualTo(rightHandSide: Double): ConstraintFormula =
     this greaterThanOrEqualTo scalar(rightHandSide)
 
-infix fun QuadraticExpression.lessThanOrEqualTo(rightHandSide: ScalarExpression): ConstraintFormula =
+infix fun QuadraticExpression.lessThanOrEqualTo(rightHandSide: RuntimeScalar): ConstraintFormula =
     QuadraticConstraintFormula(this, FormulaSense.LessThanOrEqualTo, rightHandSide)
 
 infix fun QuadraticExpression.lessThanOrEqualTo(rightHandSide: Double): ConstraintFormula =
     this lessThanOrEqualTo scalar(rightHandSide)
 
-infix fun QuadraticExpression.greaterThanOrEqualTo(rightHandSide: ScalarExpression): ConstraintFormula =
+infix fun QuadraticExpression.greaterThanOrEqualTo(rightHandSide: RuntimeScalar): ConstraintFormula =
     QuadraticConstraintFormula(this, FormulaSense.GreaterThanOrEqualTo, rightHandSide)
 
 infix fun QuadraticExpression.greaterThanOrEqualTo(rightHandSide: Double): ConstraintFormula =
